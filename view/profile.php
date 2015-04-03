53503c15c0fd2f6ff665f4305094c6bbee61931d
Yuriy <zet9@bk.ru>
2015-04-04 00:52:10 +0300

    #7

--- view/profile.php
+++ view/profile.php
@@ -23,55 +23,63 @@
 	";
 	} else {
 ?>
+<div class="wrap">
 <h1>My page</h1>
 		
-		<?php if ($result['img']=='') { $scr='avatar.png'; } else $scr=$result['img'];?>
-			<img src="/img/<?=$scr?>">
+		<!--<?php if ($result['img']=='') { $scr='avatar.png'; } else $scr=$result['img'];?>
+			<img src="/img/<?=$scr?>">-->
+	<div class="first-form">
+		<form method='post'>
+				<div class="field">
+					<label>Имя</label>
+					<input name="name" type="text" value="<?=$result['name'];?>">
+				</div>
+				<div class="field">
+					<label>Логин в играх</label>
+					<input name="nicname" type="text" value="<?=$result['nicgame']; ?>">
+				</div>
+	`			<div class="field">
+					<label>дата регистрации</label>
+					<label><?=$result['dreg'];?></label>
+				</div>
+				<div class="field">
+					<label>Игры</label>
+					<label><?php 
+					$game = json_decode($result['game'], true);?>
+					<select name="game[]" data-placeholder="Line" class="chosen-select" multiple style="width:300px;" tabindex="1">
+					<?php 
+						$games = call("SELECT * FROM `game`");
+						$i=0;
+						foreach($games as $value) {
+							if($game[$i] == $value['game']){
+								echo "<option value='".$value['game']."' selected>".$value['game']."</option>";
+							} else echo "<option value='".$value['game']."'>".$value['game']."</option>";
+							$i++;
+						}?>
+					</select>
+					</label>
+				</div>
+				<div >
+						<input class="button" name="saveinf" type="submit" value="Сохранить">
+				</div>
+		</form>
+	</div>
+	<div class="second-form">
 	<form method='post'>
-			<p>
-			<div>
-				<label>Имя</label>
-				<input name="name" type="text" value="<?=$result['name'];?>"><br />
-			</div>
-			</p>
-			<p>
-			<div>
-				<label>Логин в играх</label>
-				<input name="nicname" type="text" value="<?=$result['nicgame']; ?>"><br />
-			</div>
-			</p>
-
-			<label>дата регистрации</label>
-			<label><?=$result['dreg'];?></label><br />
-				
-			<label>Игры</label>
-			<label><?php 
-			$game = json_decode($result['game'], true);?>
-			<select name="game[]" data-placeholder="Line" class="chosen-select" multiple style="width:300px;" tabindex="1">
-			<?php 
-				$games = call("SELECT * FROM `game`");
-				$i=0;
-				foreach($games as $value) {
-					if($game[$i] == $value['game']){
-						echo "<option value='".$value['game']."' selected>".$value['game']."</option>";
-					} else echo "<option value='".$value['game']."'>".$value['game']."</option>";
-					$i++;
-				}?>
-			</select>
-			</label><br />
-			
-			<input name="saveinf" type="submit" value="Сохранить">
-	</form>
-	
-	<div>
-	<form method='post'>
-		<label>Новый пароль</label>
-		<input name="pass" type="password"><br />
-		<label>Повторите новый пароль</label>
-		<input name="pass2" type="password"><br />
-		<label>Текущий пароль</label>
-		<input name="pass3" type="password"> <br />
-		<input name="newpass" type="submit" value="Update">
+		<div class="field">
+			<label>Новый пароль</label>
+			<input name="pass" type="password">
+		</div>
+		<div class="field">
+			<label>Повторите новый пароль</label>
+			<input name="pass2" type="password">
+		</div>
+		<div class="field">
+			<label>Текущий пароль</label>
+			<input name="pass3" type="password"> 
+		</div>
+		<input class="button" name="newpass" type="submit" value="Update">
 	</form>
 	</div>
+</div>
 	<? } ?>
