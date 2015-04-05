dc4f4056e6a40256dd2460eedf1a728ddc19090b
Yuriy <zet9@bk.ru>
2015-04-05 12:51:17 +0300

    #27

--- view/tournaments.php
+++ view/tournaments.php
@@ -1,11 +1,11 @@
 <div class="wrap">
-	<div class="block-tidings">
+	<div class="tournaments">
 			<?php foreach($result as $value) { ?>
 				<div class="link" style="background-image:url(<?php echo $value['img']; ?>);">
 					<a href="/tournaments/<?php echo $value['id']; ?>">
-						<span class="title"><?php echo $value['title'];?></span>
-						<span class="title"><?php echo $value['demotitle'];?></span>
-						<span class="new-date"><?php echo date_format(date_create($value['date']), 'd.m.y');?></span>
+						<span ><?php echo $value['title'];?></span>
+						<span ><?php echo $value['demotitle'];?></span>
+						<span ><?php echo date_format(date_create($value['date']), 'd.m.y');?></span>
 					</a>
 				</div>
 			<?php } ?>
