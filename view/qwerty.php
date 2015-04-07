9e4d3ba4654d3d94580b91951b8cec83f5bdc205
Yuriy <zet9@bk.ru>
2015-04-08 00:37:37 +0300

    #41

--- view/qwerty.php
+++ view/qwerty.php
@@ -1,32 +1,34 @@
 <div class="wrap">
-	<form name=turreg method=post>
-		<div class="admin-block">
-			<label></label>
-			<input type="text" name=""> 
-		</div>
-		<div class="admin-block">
-			<label></label>
-			<input type="text" name=""> 
-		</div>
-		<div class="admin-block">
-			<label></label>
-			<select></select>
-		</div>
-		<div class="admin-block">
-			<label></label>
-			<textarea></textarea>
-		</div>
-		<div class="admin-block">
-			<label></label>
-			<textarea></textarea>
-		</div>
-		<div class="admin-block">
-			<label></label>
-			<input type="text" name=""> 
-		</div>
-		<div class="admin-block">
-			<label></label>
-			<select></select>
-		</div>
-	</form>
+	<div class="admin-form">
+		<form name=turreg method=post>
+			<div class="admin-block">
+				<label></label>
+				<input type="text" name=""> 
+			</div>
+			<div class="admin-block">
+				<label></label>
+				<input type="text" name=""> 
+			</div>
+			<div class="admin-block">
+				<label></label>
+				<select></select>
+			</div>
+			<div class="admin-block">
+				<label></label>
+				<textarea></textarea>
+			</div>
+			<div class="admin-block">
+				<label></label>
+				<textarea></textarea>
+			</div>
+			<div class="admin-block">
+				<label></label>
+				<input type="text" name=""> 
+			</div>
+			<div class="admin-block">
+				<label></label>
+				<select></select>
+			</div>
+		</form>
+	</div>
 </div>
